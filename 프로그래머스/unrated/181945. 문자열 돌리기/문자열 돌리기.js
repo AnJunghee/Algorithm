aa09a593c62a435ec73fb4c7b7e097f45eee68fe
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-01 14:56:43 +0900

    [unrated] Title: 문자열 돌리기, Time: 57.66 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    const arr = input[0].split('');
+    for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+    }
+});
