73e9650708d8eeaeb5efff8c00d4e71b90a00c40
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-02 10:57:54 +0900

    [unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,9 @@
+function solution(myString, pat) {
+    let count = 0;
+    
+    for(let i = 0; i < myString.length; i++) {
+        if(myString.substr(i, pat.length).includes(pat)) count++;
+    }
+    
+    return count;
+}
