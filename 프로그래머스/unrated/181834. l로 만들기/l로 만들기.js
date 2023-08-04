c593be24b9c8130119d3a6d18dadd4324627335e
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-04 17:30:49 +0900

    [unrated] Title: l로 만들기, Time: 8.47 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.js
@@ -1,4 +1,4 @@
 function solution(myString) {
-    
     return [...myString].map((v) => v.charCodeAt() < "l".charCodeAt() ? v = "l" : v ).join('');
+    // return myString.replace(/[a-k]/g,'l');
 }
