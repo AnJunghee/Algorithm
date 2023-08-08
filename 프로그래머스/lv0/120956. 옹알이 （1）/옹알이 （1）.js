8496b82e6265aaef46cde3441af5b94ff7ceea9a
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-08 14:46:28 +0900

    [level 0] Title: 옹알이 (1), Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -2,7 +2,7 @@ function solution(babbling) {
     let answer = 0;
     for (var i = 0 ; i < babbling.length ; i++ ) {
         if(babbling[i].replace("ye"," ").replace("aya"," ").replace("woo"," ").replace("ma"," ").trim() == "") {
-            answer = answer + 1 ;
+            answer++;
         }    
     } 
     return answer;
