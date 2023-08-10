20324873e86c55c1b1bc07f9b83225248b161865
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-10 11:18:45 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    const eng = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    for(let i = 0; i < eng.length; i++) {
+        numbers = numbers.replaceAll(eng[i], i);
+    }
+    
+    return +numbers;
+    
+}
