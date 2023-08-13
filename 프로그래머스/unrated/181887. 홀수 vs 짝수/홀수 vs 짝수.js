e0ae2b908ad0816587717e8e31f774a706278c78
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-13 23:16:12 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    
+    num_list.forEach((v, i) => {
+        i % 2 === 0 ? even += v : odd += v;
+    });
+    
+    return odd > even ? odd : even;
+}
