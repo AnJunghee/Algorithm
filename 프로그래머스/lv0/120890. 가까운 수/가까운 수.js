b1940004da1271c078a98c00ca77e54e3cf1f09d
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-14 14:45:07 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,19 +1,8 @@
 function solution(array, n) {
-    array.push(n);  // n을 배열에 삽입
-    array.sort((a, b) => a - b); // 오름차순 정렬
+    array.sort((a, b) => a - b);
+    const arr = array.map((v) => Math.abs(v - n));
+    const value = Math.min(...arr);
+    const index = arr.indexOf(value);
     
-    const idx = array.indexOf(n); // n의 인덱스
-    
-    // 앞에 요소가 없을 경우
-    if(array[idx-1] === undefined) return array[idx+1]
-    // 뒤에 요소가 없을 경우
-    else if(array[idx+1] === undefined) return array[idx-1]
-    // 앞, 뒤 요소 둘 다 있을 경우
-    else {
-        const before = array[idx] - array[idx-1];
-        const after = array[idx+1] - array[idx];
-        
-        // 더 가까운 요소 출력
-        return before <= after ? array[idx-1] : array[idx+1];
-    }
+    return array[index];
 }
