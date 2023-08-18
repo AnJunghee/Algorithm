bb065f5d1d69c85d4a3dfedeb9e6d09764774dce
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-18 15:08:33 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,9 +1,3 @@
 function solution(numlist, n) {
-    let answer = [];
-    const arr = numlist.map((v) => [Math.abs(v - n), v]);
-    
-    arr.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
-    arr.forEach(([_, v]) => answer.push(v));
-    
-    return answer;
+    return numlist.sort((a, b) => Math.abs(a - n) - Math.abs(b - n) || b - a);
 }
