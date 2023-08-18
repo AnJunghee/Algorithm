8e354eb30a8f31302c0948d3a2ff30279da6efd4
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-18 14:18:39 +0900

    [level 0] Title: 등수 매기기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,6 @@
+function solution(score) {
+    const average = score.map(([eng, math]) => (eng + math) / 2);
+    const sortedAverage = [...average].sort((a, b) => b - a);
+    
+    return average.map((v) => sortedAverage.indexOf(v) + 1);
+}
