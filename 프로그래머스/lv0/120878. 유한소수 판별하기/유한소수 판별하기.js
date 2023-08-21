196522f30b82e9d9ffc101c32dfa23591e26097a
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-21 11:32:13 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,29 +1,15 @@
 function solution(a, b) {
     // 최대공약수 구하기
-    let gcd = 0;
+    let gcd = 1;
     
     for(let i = 1; i <= a; i++) {
         if(a % i === 0 && b % i === 0) gcd = i;
     }
     
-    // 기약분수 분모의 약수 구하기
-    let factors = [];
-    let bottom = b / gcd;
+    // 분모의 약수를 2, 5로 나누기
+    b /= gcd;
+    while(b % 2 === 0) b /= 2;
+    while(b % 5 === 0) b /= 5;
     
-    for(let i = 2; i <= bottom; i++) {
-        if(bottom % i === 0) factors.push(i);
-    }
-    
-    // 소인수 구하기
-    let primeFactors = factors.filter((v) => {
-        if(v <= 1) return false; // 1과 음수는 소수가 아님
-        
-        for(let i = 2; i <= Math.sqrt(v); i++) {
-            if(v % i === 0) return false; // 다른 수로 나누어떨어지면 소수가 아님
-        }
-        
-        return true;
-    });
-    
-    return primeFactors.filter((v) => v !== 2 && v !== 5).length === 0 ? 1 : 2;
+    return b === 1 ? 1 : 2;
 }
