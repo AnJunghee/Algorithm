ff5c35c067a69bbe96eb04552c66ab6c2a6268bd
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-21 14:08:37 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,12 @@
+function solution(lines) {
+    let arr = [];
+    
+    lines.forEach(([x, y]) => {
+        for(let i = x; i < y; i++) {
+            arr.push(i.toString() + (i+1).toString());
+        }
+    });
+    
+    const filteredArray = arr.sort().filter((v) => arr.indexOf(v) !== arr.lastIndexOf(v));
+    return [...new Set(filteredArray)].length;
+}
