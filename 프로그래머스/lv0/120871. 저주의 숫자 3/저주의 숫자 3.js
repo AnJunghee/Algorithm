d30e1bdcb945614ec71199a4da3a31d80dab7e70
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-22 10:01:41 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let count = 1;
+    
+    for(let i = 1; i <= n; i++) {
+        while (count % 3 === 0 || count.toString().includes("3")) {
+            count++;
+        }
+        count++;
+    }
+    
+    return count - 1; // 마지막에 한 번 더 증가시키므로 1을 빼줍니다.
+}
