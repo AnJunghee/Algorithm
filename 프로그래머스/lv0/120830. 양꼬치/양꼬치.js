befadc2d3d12d0094e70b9469739f19980307dc4
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-28 15:11:44 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,11 +1,3 @@
 function solution(n, k) {
-    var price1 = 12000 * n;
-    var price2 = 2000 * k;
-    var service = 0;
-    for(var i=1; i<=n; i++){
-        if(i%10==0){
-            service+=1;
-        }
-    }
-    return price1 + price2 - (service*2000);
+    return (n * 12000) + (k * 2000) - (parseInt(n / 10) * 2000) ;
 }
