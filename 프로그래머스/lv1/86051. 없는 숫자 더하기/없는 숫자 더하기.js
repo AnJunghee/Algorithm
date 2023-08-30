fae372f28c22b01ef7bd3e9c748ef76ab9c22e60
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-30 10:34:30 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-    const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    numbers.forEach((v) => num.splice(num.indexOf(v), 1));
-    return num.reduce((acc, curr) => acc + curr);
+    const set = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    numbers.forEach((v) => set.delete(v));
+    return [...set].reduce((acc, curr) => acc + curr);
 }
