7f47ade7fc12374dfef437ca115ca40e2f131cc3
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-30 14:17:57 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,7 +1,3 @@
 function solution(n) {
-    let result = "";
-    for(let i = 0; i < n; i++) {
-        result += i % 2 ? "박" : "수"
-    }
-    return result;
+    return '수박'.repeat(n/2) + (n % 2 ? '수' : '');
 }
