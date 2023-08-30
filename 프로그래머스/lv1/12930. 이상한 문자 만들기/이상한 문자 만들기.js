6b7473bb52f39c37d3753e6dbd1fd49400f6fd00
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-08-30 16:53:09 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,5 +1,3 @@
-function solution(s) {
-    const arr = s.split(" ");
-    
-    return arr.map((v) => [...v].map((char, i) => i % 2 ? char.toLowerCase() : char.toUpperCase()).join("")).join(" ");
+function solution(s) {    
+    return s.split(" ").map((v) => [...v].map((char, i) => i % 2 ? char.toLowerCase() : char.toUpperCase()).join("")).join(" ");
 }
