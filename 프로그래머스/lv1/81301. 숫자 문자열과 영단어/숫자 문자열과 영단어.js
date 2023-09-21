8936b930858516bdd322ac57e03150b2fb7ae019
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-09-21 17:15:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,15 +1,15 @@
 function solution(s) {
     let answer = '';
-    answer = s.replaceAll(/zero/g, 0);
-    answer = answer.replaceAll(/one/g, 1);
-    answer = answer.replaceAll(/two/g, 2);
-    answer = answer.replaceAll(/three/g, 3);
-    answer = answer.replaceAll(/four/g, 4);
-    answer = answer.replaceAll(/five/g, 5);
-    answer = answer.replaceAll(/six/g, 6);
-    answer = answer.replaceAll(/seven/g, 7);
-    answer = answer.replaceAll(/eight/g, 8);
-    answer = answer.replaceAll(/nine/g, 9);
+    
+    answer = s.replaceAll(/zero/g, 0)
+        .replaceAll(/one/g, 1)
+        .replaceAll(/two/g, 2)
+        .replaceAll(/three/g, 3)
+        .replaceAll(/four/g, 4)
+        .replaceAll(/five/g, 5)
+        .replaceAll(/six/g, 6)
+        .replaceAll(/seven/g, 7)
+        .replaceAll(/eight/g, 8).replaceAll(/nine/g, 9);
     
     return Number(answer);
 }
