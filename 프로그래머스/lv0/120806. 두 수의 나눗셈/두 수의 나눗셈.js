b380476471dbcfb17be67b7670b150fc37728dfd
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-10-01 00:19:40 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,6 @@
+function solution(num1, num2) {
+    var answer = (num1/num2)*1000;
+    return parseInt(answer);
+    //const solution = (num1, num2) => Math.floor(num1 / num2 * 1000)
+    //return Math.trunc(num1 / num2 * 1000);
+}
