09465656126956a152ad229ed5ccbc3c5e75f81b
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-11-01 14:50:14 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,10 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+solution();
+
+function solution() {
+    console.log('\\    /\\');
+    console.log(' )  ( \')');
+    console.log('(  /  )');
+    console.log(' \\(__)|');
+}
