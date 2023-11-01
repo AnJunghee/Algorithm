6f75c280e8e258558383f52a7636ddc868dcbb23
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-11-01 15:02:16 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,11 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+solution(input);
+
+function solution(n) {
+    if(n >= 90) console.log('A');
+    else if(n >= 80) console.log('B');
+    else if(n >= 70) console.log('C');
+    else if(n >= 60) console.log('D');
+    else console.log('F');
+}
