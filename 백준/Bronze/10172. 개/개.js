59c1ea307acf3f1f32dabc8f5ea7d43d70724fe5
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-11-01 14:54:55 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,11 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+solution();
+
+function solution() {
+    console.log('|\\_/|');
+    console.log('|q p|   /}');
+    console.log('( 0 )"""\\');
+    console.log('|"^"`    |');
+    console.log('||_/=\\\\__|');
+}
