aaa05dbaf7f461e670dfb03a4d6ac64f688ad958
안정희 <80940078+AnJunghee@users.noreply.github.com>
2023-11-06 10:17:17 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,4 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
+
+console.log(Math.max(...input));
+console.log(input.indexOf(Math.max(...input))+1);
